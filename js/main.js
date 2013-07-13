12576d0853fe1f31bfe6c9f49112f20af51965c1
Ricardo García Fernández <ricardogarfe@gmail.com>
2013-07-13 14:23:43 +0200

    JS API Client Projects and Users

--- js/main.js
+++ js/main.js
@@ -3,13 +3,27 @@ $.ajax({url: 'http://10.71.199.207:27080/science_project/users/_find',
     dataType: "JSONP",
     success: function( data ) {
 
-    var userNames = [];
+	    var userNames = [];
 
-    $.each( data.results, function( i, result ) {
-        var userNameString = "<p>" + result.username + "</p>"
-        userNames.push(userNameString);
-    });
+	    $.each( data.results, function( i, result ) {
+	        var userNameString = "<p>" + result.username + "</p>"
+	        userNames.push(userNameString);
+	    });
+	}
+});
 
-    $("myDiv").append(userNames);
+$.ajax({url: 'http://10.71.199.207:27080/science_project/projects/_find',
+    type: 'GET',
+    dataType: "JSONP",
+    success: function( data ) {
+
+	    var projecNames = [];
+
+	    $.each( data.results, function( i, result ) {
+	        var projectNameString = "<p>" + result.username + "</p>"
+	        projectNames.push(projectNameString);
+	    });
+
+	    $("projectList").append(projectNames);
 	}
 });
