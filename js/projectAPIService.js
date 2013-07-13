c5ff60227b4885eb7ac6752952a3969dafe8475d
Ricardo García Fernández <ricardogarfe@gmail.com>
2013-07-13 16:41:22 +0200

    Insert new user API.

--- js/projectAPIService.js
+++ js/projectAPIService.js
@@ -1,13 +1,14 @@
 function retrieveProjects () {
 	$.ajax({url: urlAPI + databaseSchema + '/projects/_find',
 	    type: 'GET',
-	    dataType: "JSONP",
+	    dataType: "JSON",
 	    success: function( data ) {
 
 		    var projectNames = [];
 
 		    $.each( data.results, function( i, result ) {
 		        var projectNameString = "<p>" + result.name + "</p>"
+		        console.log(projectNameString);
 		        projectNames.push(projectNameString);
 		    });
 
@@ -19,7 +20,7 @@ function retrieveProjects () {
 function findProjectByCriteria(criteria) {
 	$.ajax({url: urlAPI + databaseSchema + '/projects/_find',
 	    type: 'GET',
-	    dataType: "JSONP",
+	    dataType: "JSON",
 	    data: '?criteria= '+ criteria,
 	    success: function( data ) {
 
@@ -27,6 +28,7 @@ function findProjectByCriteria(criteria) {
 
 		    $.each( data.results, function( i, result ) {
 		        var userNameString = "<p>" + result.username + "</p>"
+		        console.log(projectNameString);
 		        userNames.push(userNameString);
 		    });
 		}
--- js/userAPIService.js
+++ js/userAPIService.js
@@ -1,13 +1,14 @@
 function retrieveUsers () {
 	$.ajax({url: urlAPI + databaseSchema + '/users/_find',
 	    type: 'GET',
-	    dataType: "JSONP",
+	    dataType: "JSON",
 	    success: function( data ) {
 
 		    var userNames = [];
 
 		    $.each( data.results, function( i, result ) {
-		        var userNameString = "<p>" + result.username + "</p>"
+		        var userNameString = "<p>" + result.username + "</p>";
+		        console.log(userNameString);
 		        userNames.push(userNameString);
 		    });
 		}
@@ -17,15 +18,30 @@ function retrieveUsers () {
 function findUserByCriteria(criteria) {
 	$.ajax({url: urlAPI + databaseSchema + '/users/_find',
 	    type: 'GET',
-	    dataType: "JSONP",
+	    dataType: "JSON",
 	    data: '?criteria= '+ criteria,
 	    success: function( data ) {
 
 		    var userNames = [];
 
 		    $.each( data.results, function( i, result ) {
-		        var userNameString = "<p>" + result.username + "</p>"
+		        var userNameString = "<p>" + result.username + "</p>";
+		        console.log(userNameString);
 		        userNames.push(userNameString);
+		    });
+		}
+	});
+}
+
+function createUser(userData) {
+	$.ajax({url: urlAPI + databaseSchema + '/users/_insert',
+	    type: 'POST',
+	    dataType: "JSON",
+	    data: 'docs=[' + JSON.stringify(userData) + ']',
+	    success: function( data ) {
+
+		    $.each( data.results, function( i, result ) {
+		        console.log(result.ok);
 		    });
 		}
 	});
