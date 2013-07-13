6d0030b205fa48a9a16985c714a76008fb332850
Ricardo García Fernández <ricardogarfe@gmail.com>
2013-07-13 14:20:29 +0200

    Javascript API client

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$.ajax({url: 'http://10.71.199.207:27080/science_project/users/_find',
+    type: 'GET',
+    dataType: "JSONP",
+    success: function( data ) {
 
+    var userNames = [];
+
+    $.each( data.results, function( i, result ) {
+        var userNameString = "<p>" + result.username + "</p>"
+        userNames.push(userNameString);
+    });
+
+    $("myDiv").append(userNames);
+	}
+});
